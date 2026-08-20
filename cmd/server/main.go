2902e6d2658fd1bc2a53f191bbad413ec2f02756
belyaevEDU <veniamin@belyaev.work>
2026-08-20 16:15:57 +0300

    feat: added main

---
+++ cmd/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+
+	"github.com/belyaevedu/remote-code-service/internal/config"
+	"github.com/belyaevedu/remote-code-service/internal/controller"
+	"github.com/belyaevedu/remote-code-service/internal/controller/handlers"
+	"github.com/belyaevedu/remote-code-service/internal/repository"
+	"github.com/belyaevedu/remote-code-service/internal/service"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("invalid config: %v", err)
+	}
+
+	repo := repository.New()
+	taskService := service.New(repo, cfg.ProcessingTime)
+	taskHandler := handlers.New(taskService)
+	router := controller.NewRouter(taskHandler)
+	server := controller.NewApi(cfg.HTTPAddr, router, cfg.ShutdownTimeout)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer stop()
+
+	if err := server.Start(ctx); err != nil {
+		log.Fatalf("server exited with error: %v", err)
+	}
+}
