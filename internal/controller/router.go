ede5dd967422ac106f1d468e90b68c682d0c810f
belyaevEDU <veniamin@belyaev.work>
2026-08-21 16:17:22 +0300

    feat(controller/router): using new auth middleware and user handlers

--- internal/controller/router.go
+++ internal/controller/router.go
@@ -1,17 +1,24 @@
 package controller
 
 import (
-	"github.com/belyaevedu/remote-code-service/internal/controller/handlers"
-
 	"github.com/go-chi/chi/v5"
+
+	"github.com/belyaevedu/remote-code-service/internal/controller/handlers"
+	"github.com/belyaevedu/remote-code-service/internal/port"
 )
 
-func NewRouter(h *handlers.TaskHandlers) *chi.Mux {
+func NewRouter(t *handlers.TaskHandlers, u *handlers.UserHandlers, auth port.AuthService) *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Post("/task", h.Create)
-	r.Get("/status/{task_id}", h.Status)
-	r.Get("/result/{task_id}", h.Result)
+	r.Post("/register", u.Register)
+	r.Post("/login", u.Login)
+
+	r.Group(func(r chi.Router) {
+		r.Use(AuthMiddleware(auth))
+		r.Post("/task", t.Create)
+		r.Get("/status/{task_id}", t.Status)
+		r.Get("/result/{task_id}", t.Result)
+	})
 
 	return r
 }
