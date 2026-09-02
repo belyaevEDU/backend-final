a61d4bc568accd3f3816565abd948c02f3acba21
belyaevEDU <veniamin@belyaev.work>
2026-09-03 02:22:03 +0300

    fix(config/app): removed unnecessary fields

--- internal/config/app.go
+++ internal/config/app.go
@@ -7,34 +7,27 @@ import (
 
 type AppConfig struct {
 	HTTPAddr        string
-	ProcessingTime  time.Duration
 	ShutdownTimeout time.Duration
 }
 
 const (
 	envVarAppAddress         = "HTTP_ADDR"
-	envVarAppProcessingTime  = "TASK_PROCESSING_TIME"
 	envVarAppShutdownTimeout = "SHUTDOWN_TIMEOUT"
 )
 
 const (
 	defaultAppAddress         = ":8000"
-	defaultAppProcessingTime  = 2 * time.Second
 	defaultAppShutdownTimeout = 10 * time.Second
 )
 
 func LoadAppConfig() (AppConfig, error) {
 	appCfg := AppConfig{
 		HTTPAddr:        envString(envVarAppAddress, defaultAppAddress),
-		ProcessingTime:  envDuration(envVarAppProcessingTime, defaultAppProcessingTime),
 		ShutdownTimeout: envDuration(envVarAppShutdownTimeout, defaultAppShutdownTimeout),
 	}
 
-	if appCfg.ProcessingTime <= 0 {
-		return AppConfig{}, fmt.Errorf("TASK_PROCESSING_TIME must be positive, got %s", appCfg.ProcessingTime)
-	}
 	if appCfg.ShutdownTimeout <= 0 {
-		return AppConfig{}, fmt.Errorf("SHUTDOWN_TIMEOUT must be positive, got %s", appCfg.ShutdownTimeout)
+		return AppConfig{}, fmt.Errorf("%s must be positive, got %s", envVarAppShutdownTimeout, appCfg.ShutdownTimeout)
 	}
 	return appCfg, nil
 }
